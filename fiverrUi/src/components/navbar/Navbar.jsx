9aaf33cb55e8452664aca8480983c6057fc5d3fa
barakaganai <barakaganai01@gmail.com>
2023-03-31 11:58:34 +0300

    create navbar.jsx

---
+++ fiverrUi/src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import "./Navbar.scss";
+// import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="textLogo">Fiverr</span>
+          {/* </Link> */}
+
+          <span className="dotLogo">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Become a seller</span>
+          <span>Sign In</span>
+          <button>Join</button>
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>test</span>
+            <span>test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
