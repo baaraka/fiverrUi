5b8686c46bc8210793731ecf265b7c0b4600a164
barakaganai <barakaganai01@gmail.com>
2023-04-01 17:39:05 +0300

    install the react router dom

--- fiverrUi/src/App.jsx
+++ fiverrUi/src/App.jsx
@@ -1,10 +1,36 @@
 import Navbar from "./components/navbar/Navbar";
 import "./App.scss";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
