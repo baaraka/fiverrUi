fe3c184c7eb65f5985a6c7a2a691352ee2d3c786
barakaganai <barakaganai01@gmail.com>
2023-04-02 11:57:32 +0300

    import trustedBy to home component

--- fiverrUi/src/components/home/Home.jsx
+++ fiverrUi/src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from "../featured/Featured";
+import TrustedBy from "../trustedBy/TrustedBy";
 import "./Home.scss";
 
 function Home() {
   return (
     <div>
       <Featured />
+      <TrustedBy />
     </div>
   );
 }
