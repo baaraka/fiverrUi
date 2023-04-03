d084b8747ac2d67504da9d63d75d6dd30599a2ac
barakaganai <barakaganai01@gmail.com>
2023-04-03 10:52:43 +0300

    import slide component into the home component

--- fiverrUi/src/components/home/Home.jsx
+++ fiverrUi/src/components/home/Home.jsx
@@ -1,12 +1,20 @@
 import Featured from "../featured/Featured";
+import Slide from "../slide/Slide";
 import TrustedBy from "../trustedBy/TrustedBy";
 import "./Home.scss";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
+      </Slide>
     </div>
   );
 }
