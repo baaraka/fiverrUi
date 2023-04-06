578c5b4220c4caff12e6e3a0a5d0e1e770a3befe
barakaganai <barakaganai01@gmail.com>
2023-04-06 20:10:26 +0300

    create Orders.jsx file

---
+++ fiverrUi/src/pages/orders/Orders.jsx
@@ -0,0 +1,130 @@
+import "./Orders.scss";
+import { Link } from "react-router-dom";
+
+function Orders() {
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Orders</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
