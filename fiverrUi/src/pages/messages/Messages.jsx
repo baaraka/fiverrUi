38db84feb24f793ce7f7610f31209e1e09127768
barakaganai <barakaganai01@gmail.com>
2023-04-07 12:31:26 +0300

    create messages.jsx page

---
+++ fiverrUi/src/pages/messages/Messages.jsx
@@ -0,0 +1,95 @@
+import "./Messages.scss";
+import { Link } from "react-router-dom";
+
+function Messages() {
+  const message = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis temporibus praesentium quae iusto tenetur enim omnis, odio dolorem possimus exercitationem dolorum a porro! Repudiandae et culpa in animi, aliquid optio.`;
+
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>
+              <Link to="/message" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 70)}...
+              </Link>
+            </td>
+            <td>1 day ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Messages;
