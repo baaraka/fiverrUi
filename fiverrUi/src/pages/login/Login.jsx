18789bddabd3b3db7ab7837f205cc76539b9ca44
barakaganai <barakaganai01@gmail.com>
2023-04-19 16:16:37 +0300

    create login.jsx file

---
+++ fiverrUi/src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import "./Login.scss";
+
+function Login() {
+  return (
+    <div className="login">
+      <form>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input name="username" type="text" placeholder="john doe" />
+        <label htmlFor="">Password</label>
+        <input name="password" type="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
