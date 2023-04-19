6e52c8aebf91a9e6f501dc276cbefdeb4c199298
barakaganai <barakaganai01@gmail.com>
2023-04-19 18:04:55 +0300

    import newRequest and show current user

--- fiverrUi/src/pages/login/Login.jsx
+++ fiverrUi/src/pages/login/Login.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 import "./Login.scss";
-import axios from "axios";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
-        {
-          username,
-          password,
-        },
-        { withCredentials: true }
-      );
-      console.log(res.data);
+      const res = await newRequest.post("auth/login", { username, password });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/");
     } catch (err) {
-      setError(err);
-      console.log(err);
+      setError(err.response.data);
     }
   };
 
@@ -43,6 +39,7 @@ function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Login</button>
+        {error && error}
       </form>
     </div>
   );
