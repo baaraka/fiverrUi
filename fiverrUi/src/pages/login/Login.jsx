94addd99161fe16c7263ebe4bf1bd54e6d669487
barakaganai <barakaganai01@gmail.com>
2023-04-19 17:09:36 +0300

    implement the functionality to enable login

--- fiverrUi/src/pages/login/Login.jsx
+++ fiverrUi/src/pages/login/Login.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import "./Login.scss";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(res.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Sign in</h1>
         <label htmlFor="">Username</label>
-        <input name="username" type="text" placeholder="john doe" />
+        <input
+          name="username"
+          type="text"
+          placeholder="john doe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label htmlFor="">Password</label>
-        <input name="password" type="password" />
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
     </div>
