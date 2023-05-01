aa8f84b9d851cf2bd61460c660c3dde90c71a4a3
barakaganai <barakaganai01@gmail.com>
2023-05-01 16:04:55 +0300

    implement register functionality in react

--- fiverrUi/src/pages/register/Register.jsx
+++ fiverrUi/src/pages/register/Register.jsx
@@ -1,21 +1,80 @@
+import { useState } from "react";
 import "./Register.scss";
+import axios from "axios";
+import upload from "../../utils/upload";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const [file, setFile] = useState(null);
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    img: "",
+    country: "",
+    isSeller: false,
+    desc: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const handleSeller = (e) => {
+    setUser((prev) => {
+      return { ...prev, isSeller: e.target.checked };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = await upload(file);
+
+    try {
+      await newRequest.post("/auth/register", {
+        ...user,
+        img: url,
+      });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="register">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="left">
           <h1>Create a new account</h1>
           <label htmlFor="">Username</label>
-          <input name="username" type="text" placeholder="john doe" />
+          <input
+            name="username"
+            type="text"
+            placeholder="john doe"
+            onChange={handleChange}
+          />
           <label htmlFor="">Email</label>
-          <input name="email" type="email" placeholder="email" />
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
           <label htmlFor="">Password</label>
-          <input name="password" type="password" />
+          <input name="password" type="password" onChange={handleChange} />
           <label htmlFor="">Profile Picture</label>
-          <input type="file" />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
           <label htmlFor="">Country</label>
-          <input name="country" type="text" placeholder="Tanzania" />
+          <input
+            name="country"
+            type="text"
+            placeholder="Tanzania"
+            onChange={handleChange}
+          />
           <button type="submit">Register</button>
         </div>
         <div className="right">
@@ -23,12 +82,17 @@ function Register() {
           <div className="toggle">
             <label htmlFor="">Activate the seller account</label>
             <label className="switch">
-              <input type="checkbox" />
+              <input type="checkbox" onChange={handleSeller} />
               <span className="slider round"></span>
             </label>
           </div>
           <label htmlFor="">Phone Number</label>
-          <input name="phone" type="text" placeholder="+255 768 086 183" />
+          <input
+            name="phone"
+            type="text"
+            placeholder="+255 768 086 183"
+            onChange={handleChange}
+          />
           <label htmlFor="">Description</label>
           <textarea
             placeholder="A short description of yourself"
@@ -36,6 +100,7 @@ function Register() {
             id=""
             cols="30"
             rows="10"
+            onChange={handleChange}
           ></textarea>
         </div>
       </form>
